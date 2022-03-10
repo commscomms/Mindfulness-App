f061f280866d0eb45833a290fd8a255801254cc1
commscomms <edesa23@student.dimanregional.org>
2022-03-10 12:04:33 -0500

    Mindfulness App
    e

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let seconds = 30;
+let myInterval;
+let isRunning = false;
+
+function start() {
+    if (isRunning == false) {
+        isRunning = true;
+        const time = document.getElementById("countdown");
+        console.log("start");
+        myInterval = setInterval(() => {
+            time.innerHTML = seconds--;
+            if (seconds < 0) clearInterval(myInterval);
+        }, 1000);
+    }
+    else {
+            if (seconds == 0) {
+                isRunning = false;
+            }
+        }
+}
+
+function stop() {
+    isRunning = false;
+    clearInterval(myInterval);
+    console.log("stop");
+  }
+
+function reset() {
+    console.log("reset");
+    seconds = 10;
+}
